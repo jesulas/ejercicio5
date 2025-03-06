50d794c82adbc22a96e1468103c484d71dab4030
Jesús <jesusdeabreunavarro1@gmail.com>
2025-03-06 12:31:56 +0000

    Arreglado el ejercicio 5

--- src/main.js
+++ src/main.js
@@ -1,157 +1,139 @@
-import "./style.css";
 
-console.log("Hello from main");
-let CartaRobar = 1;
-let baraja = [4,4,4,4,4,4,4,4,4,4];
-let puntuacion = 0;
-let NumeroPuntos = document.getElementById("puntos");
-let plantado = false;
-let reset = document.getElementById('resetear');
-const goal = 7.5;
-console.log(CartaRobar);
+console.log("se abrea")
+const partida = {
+    puntosTotales:0
+}
 
-function generarCarta(){
-if (plantado == true){
-return
+const botonRobarCarta = document.getElementById("robarCarta");
+if ((botonRobarCarta != null && botonRobarCarta != undefined)){
+  botonRobarCarta.addEventListener("click", robarCarta)
+  }
+const botonPlantarJuego = document.getElementById("plantarJuego");
+if ((botonPlantarJuego != null && botonPlantarJuego != undefined)){
+  botonPlantarJuego.addEventListener("click", plantado)
 }
-CartaRobar = Math.floor(Math.random()*10)
-console.log(CartaRobar);
-//Evita robar mas de 4 veces la misma carta
-if ((baraja[CartaRobar]<1)){
-generarCarta();
-return;
-} else {
-baraja[CartaRobar]--;
-console.log(baraja);    
+const botonReiniciar = document.getElementById("reiniciar");
+if ((botonReiniciar != null && botonReiniciar != undefined)){
+  botonReiniciar.addEventListener("click", reiniciar)
 }
-var img = document.createElement("img");
-    img.id = "espalda";
-    img.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg"; 
-    var src = document.getElementById("x"); 
-    src.appendChild(img);
+
+
+function numeroAleatorio(){
+    return (Math.floor(Math.random()*(10)))
 }
-// ---------------------------------------------------------//
-//  Partir esto entre 2 funciones para usar la boca abajo   //
-// ---------------------------------------------------------//
-function revelarCarta(){
-    if (plantado == true){
-        return
+
+function dameCarta(numeroAleatorio){
+    if (numeroAleatorio > 7) {
+      return numeroAleatorio + 2;
     }
-    var img = document.getElementById("espalda");
-    img.parentNode.removeChild(espalda);
-switch(CartaRobar){
-case 0:
-    var img = document.createElement("img"); 
-    img.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg"; 
-    var src = document.getElementById("x"); 
-    src.appendChild(img);
-    puntuacion++;
-    break;
-case 1:
-    var img = document.createElement("img"); 
-    img.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg"; 
-    var src = document.getElementById("x"); 
-    src.appendChild(img);
-    puntuacion = puntuacion + 2;
-    break;
-case 2:
-    var img = document.createElement("img"); 
-    img.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg"; 
-    var src = document.getElementById("x"); 
-    src.appendChild(img);
-    puntuacion = puntuacion + 3;
-    break;
-case 3:
-    var img = document.createElement("img"); 
-    img.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg"; 
-    var src = document.getElementById("x"); 
-    src.appendChild(img);
-    puntuacion = puntuacion + 4;
-    break;
-case 4:
-    var img = document.createElement("img"); 
-    img.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg"; 
-    var src = document.getElementById("x"); 
-    src.appendChild(img);
-    puntuacion = puntuacion + 5;
-    break;
-case 5:
-    var img = document.createElement("img"); 
-    img.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg"; 
-    var src = document.getElementById("x"); 
-    src.appendChild(img);
-    puntuacion = puntuacion + 6;
-    break;
-case 6:
-    var img = document.createElement("img"); 
-    img.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg"; 
-    var src = document.getElementById("x"); 
-    src.appendChild(img);
-    puntuacion = puntuacion + 7;
-    break;
-case 7:
-    var img = document.createElement("img"); 
-    img.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg"; 
-    var src = document.getElementById("x"); 
-    src.appendChild(img);
-    puntuacion = puntuacion + 0.5;
-    break;
-case 8:
-    var img = document.createElement("img"); 
-    img.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg"; 
-    var src = document.getElementById("x"); 
-    src.appendChild(img);
-    puntuacion = puntuacion + 0.5;
-    break;
-case 9:
-    var img = document.createElement("img"); 
-    img.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg"; 
-    var src = document.getElementById("x"); 
-    src.appendChild(img);
-    puntuacion = puntuacion + 0.5;
-    break;
-}
-NumeroPuntos.textContent = (puntuacion)
-if (puntuacion > goal){
- alert("Ha pasado el limite, ha perdido el juego.");
- plantado = true;
- reset.style.display = 'block';
-}
+    if (numeroAleatorio < 1){
+      return 1;
+    }
+    return numeroAleatorio;
+  }
+
+function dameURLCarta(carta){
+    switch(carta){
+      case 1:
+          return ("https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg") 
+      case 2:
+          return ("https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg"); 
+      case 3:
+          return ("https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg"); 
+      case 4:
+          return ("https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg"); 
+      case 5:
+          return ("https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg"); 
+      case 6:
+          return ("https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg"); 
+      case 7:
+          return ("https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg"); 
+      case 9:
+          return ("https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg"); 
+      case 10:
+          return ("https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg"); 
+      case 11:
+          return ("https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg");
+      default:
+          console.log("Numero irregular.");
+          console.log(carta);
+  }
+  }
+
+function mostrarURLCarta(urlCarta){
+    const elementoImagen = document.getElementById("img");
+
+    if (elementoImagen !== null && elementoImagen !== undefined){
+      elementoImagen.src = urlCarta;
+    }
 }
 
-function plantar(){
-console.log(puntuacion);
-plantado = true;
-reset.style.display = 'block';
-if (puntuacion < 4){
-alert("Has sido muy conservador");
-} 
-else if ((puntuacion >= 4) && (puntuacion <= 5)) {
-alert("Te ha entrado el canguelo eh?");
-}
-else if ((puntuacion >5) && (puntuacion < 7.5)){
-alert("Casi casi...")   
-}
-else if ((puntuacion == 7.5)){
-alert("¡Lo has clavado!¡Enhorabuena!")
-}
-else if (puntuacion > 7.5){
-alert("Se ha pasado de el limite.")
-}
+function obtenerPuntosCarta(carta){
+    if (carta > 7){
+      return 0.5;
+    }
+    return carta;
 }
 
-function reinciar(){
-    let e = document.getElementById("x");
-        e.innerHTML = "";
-    baraja = [4,4,4,4,4,4,4,4,4,4];
-    puntuacion = 0;
-        NumeroPuntos.textContent = (puntuacion)
-    plantado = false;
-    reset.style.display = "none";
+function sumarPuntos(puntos){
+    return (partida.puntosTotales + puntos);
 }
-document.getElementById("robar").addEventListener("click", generarCarta);
-document.getElementById("revelar").addEventListener("click", revelarCarta);
-document.getElementById("plantar").addEventListener("click", plantar);
-document.getElementById("resetear").addEventListener("click",reinciar);
+
+function actualizarPuntosTotales(nuevosPuntos){
+  partida.puntosTotales = nuevosPuntos
+}
+
+function actualizarText(){
+  const scoreB = document.getElementById("scoreboard");
+    
+    if (scoreB !== null && scoreB !== undefined){ 
+      scoreB.textContent = partida.puntosTotales;
+    }
+
+}
 
 
+
+function reiniciar(){
+  actualizarPuntosTotales(0);
+  actualizarText();
+  mostrarURLCarta("");
+}
+
+function robarCarta(){
+    const numeroRandom = numeroAleatorio();
+    console.log(numeroRandom)
+    const carta = dameCarta(numeroRandom);
+    console.log(carta)
+    const urlCarta = dameURLCarta(carta);
+    mostrarURLCarta(urlCarta);
+    const puntosCarta = obtenerPuntosCarta(carta);
+    console.log(puntosCarta)
+    const puntosSumados = sumarPuntos(puntosCarta);
+    console.log(puntosSumados)
+    actualizarPuntosTotales(puntosSumados);
+    actualizarText()
+}
+
+function plantado(){
+ revisarPartida()
+}
+
+function revisarPartida(){
+
+  if(partida.puntosTotales === 7.5){
+    alert("Has ganado la partida.")
+   }
+   if(partida.puntosTotales > 7.5){
+    alert("Has perdido la partida")
+   }
+   if (partida.puntosTotales < 4){
+     alert("Has sido muy conservador");
+   } 
+   if ((partida.puntosTotales >= 4) && (partida.puntosTotales <= 5)) {
+     alert("Te ha entrado el canguelo eh?");
+   }
+   if ((partida.puntosTotales >5) && (partida.puntosTotales < 7.5)){
+     alert("Casi casi...")   
+   }
+}
